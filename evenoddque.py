90817ad2b850b0a815380c5402773220cd71367f
Khushbu Verma <vkhushbu874@gmail.com>
2025-11-07 10:44:42 +0530

    Add files via upload

---
+++ evenoddque.py
@@ -0,0 +1,8 @@
+#1. An Integer is Input through keyboard. Write a program to find
+#whether it is odd or even number.
+
+num=int(input("enter the num="))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
